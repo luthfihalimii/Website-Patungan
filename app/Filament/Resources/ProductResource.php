55461d569c82aae93429458b03679d09a11d79fb
luthfihalimii <qlatifah62@gmail.com>
2025-03-02 06:10:32 +0700

    Coding CRUD data products

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -12,6 +12,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Fieldset;
+use PhpParser\Node\Stmt\Label;
 
 class ProductResource extends Resource
 {
@@ -19,11 +21,103 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Products';
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+
+                Fieldset::make('Details')
+                ->schema([
+                    Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                    Forms\Components\TextInput::make('tagline')
+                    ->required()
+                    ->maxLength(255),
+
+                    Forms\Components\FileUpload::make('thumbnail')
+                    ->image()
+                    ->required(),
+
+                    Forms\Components\FileUpload::make('photo')
+                    ->image()
+                    ->required(),
+
+                    Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR')
+                    ->live()
+                    ->afterStateUpdated(function (callable $get, callable $set) {
+                        $price = $get('price');
+                        $capacity = $get('capacity');
+                        if ($capacity > 0) {
+                            $set ('price_per_person', $price / $capacity);
+                        } else {
+                            $set ('price_per_person', null);
+                        }
+                    }),
+
+                    Forms\Components\TextInput::make('capacity')
+                    ->required()
+                    ->numeric()
+                    ->prefix('People')
+                    ->live()
+                    ->afterStateUpdated(function (callable $get, callable $set) {
+                        $price = $get('price');
+                        $capacity = $get('capacity');
+                        if ($capacity > 0) {
+                            $set ('price_per_person', $price / $capacity);
+                        } else {
+                            $set ('price_per_person', null);
+                        }
+                    }),
+
+                    Forms\Components\TextInput::make('price_per_person')
+                    ->numeric()
+                    ->readOnly()
+                    ->prefix('IDR')
+                    ->live()
+                    ->afterStateUpdated(function (callable $get, callable $set) {
+                        $price = $get('price');
+                        $capacity = $get('capacity');
+                        if ($capacity > 0) {
+                            $set ('price_per_person', $price / $capacity);
+                        } else {
+                            $set ('price_per_person', null);
+                        }
+                    }),
+
+                    Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Month'),
+
+                ]),
+
+                    Fieldset::make('Additional')
+                    ->schema([
+
+                        Forms\Components\Textarea::make('about')
+                        ->required(),
+
+                        Forms\Components\Repeater::make('keypoints')
+                        ->relationship('keypoints')
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                            ->required(),
+                        ]),
+
+                        Forms\Components\Select::make('is_popular')
+                        ->options([
+                            true => 'Popular',
+                            false => 'Not Popular',
+                        ])
+                        ->required(),
+                        ]),
             ]);
     }
 
@@ -31,12 +125,27 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                //
+
+                Tables\Columns\ImageColumn::make('thumbnail'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\IconColumn::make('is_popular')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Popular'),
+
+
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
