82c7603bc43edf32de80668b2e6624dd491687fa
luthfihalimii <qlatifah62@gmail.com>
2025-03-02 12:12:33 +0700

    Coding CRUD data transaksi

--- app/Filament/Resources/ProductSubscriptionResource.php
+++ app/Filament/Resources/ProductSubscriptionResource.php
@@ -4,26 +4,161 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProductSubscriptionResource\Pages;
 use App\Filament\Resources\ProductSubscriptionResource\RelationManagers;
+use App\Models\Product;
 use App\Models\ProductSubscription;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Validation\Rules\Numeric;
 
 class ProductSubscriptionResource extends Resource
 {
     protected static ?string $model = ProductSubscription::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-credit-card';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 //
+
+                Forms\Components\Wizard::make([
+
+                    Forms\Components\Wizard\Step::make('Product and Price')
+                    ->schema([
+
+                        Grid::make(2)
+                        ->schema([
+                            Forms\Components\Select::make('product_id')
+                            ->relationship('product', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->live()
+                            ->afterStateUpdated(function ($state, callable $set) {
+
+                                $product = Product::find($state);
+                                $price = $product ? $product->price_per_person : 0;
+                                $duration = $product ? $product->duration : 0;
+
+                                $set('price' $price);
+                                $set('duration' $duration);
+
+                                $tax = 0.11;
+                                $totalTaxAmount = $tax * $price;
+
+                                $totalAmount = $price + $totalTaxAmount;
+                                $set('total_amount', number_format($totalAmount, 0 , '', ''));
+                                $set('total_tax_amount', number_format($totalTaxAmount, 0 , '', ''));
+
+                            })
+
+                            afterStateHydrated(function (callable $get, callable $set, $state){
+                                $productId = $state;
+                                if ($productId) {
+                                    $product = Product::find($productId);
+                                    $price = $product ? $product->price : 0;
+
+                                    $set('price', $price);
+
+                                    $tax = 0.11;
+                                    $totalTaxAmount = $tax * $price;
+
+                                    $totalAmount = $price + $totalTaxAmount;
+                                    $set('total_tax_amount', number_format($totalTaxAmount, 0 , '', ''));
+                                }
+                            }),
+
+                            Forms\Components\TextInput::make('price')
+                            ->required()
+                            ->label('Price per person')
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('total_amount')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('total_tax_amount')
+                            ->readOnly()
+                            ->required()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('duration')
+                            ->readOnly()
+                            ->required()
+                            ->numeric()
+                            ->prefix('Month'),
+                        ]),
+                    ]),
+
+                    Forms\Components\Wizard\Step::make('Customer Information')
+                    ->schema([
+
+                        Grid::make(2)
+                        ->schema([
+
+                            Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->length(255),
+
+                            Forms\Components\TextInput::make('phone')
+                            ->required()
+                            ->maxLength(255),
+
+                            Forms\Components\TextInput::make('email')
+                            ->required()
+                            ->maxLength(255),
+                        ]),
+                    ]),
+
+                    Forms\Components\Wizard\Step::make('Payment Information')
+                    ->schema([
+
+                        Forms\Components\TextInput::make('booking_trx_id')
+                        ->required()
+                        ->maxLength(255),
+
+                        Forms\Components\TextInput::make('customer_bank_name')
+                        ->required()
+                        ->maxLength(255),
+
+                        Forms\Components\TextInput::make('customer_bank_number')
+                        ->required()
+                        ->maxLength(255),
+
+                    ToggleButtons::make('is_paid')
+                        ->label('Apakah sudah membayar?')
+                        ->boolean()
+                        ->grouped()
+                        ->icons([
+                            true => 'heroicon-o-pencil',
+                            false => 'heroicon-o-clock',
+                        ])
+                        ->required(),
+
+                    Forms\Components\FileUpload::make('proof')
+                        ->image()
+                        ->required(),
+                    ]),
+
+                ])
+                ->columnSpan('full')
+                ->columns(1)
+                ->skippable()
             ]);
     }
 
@@ -32,12 +167,43 @@ class ProductSubscriptionResource extends Resource
         return $table
             ->columns([
                 //
+                Tables\Columns\ImageColumn::make('product.thumbnail')
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('Success')
+                    ->falseColor('Danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi'),
             ])
             ->filters([
+                SelectFilter::make('product_id')
+                    ->label('Product')
+                    ->relationship('product', 'name')
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('Approve'),
+                ->label('Approve')
+                ->action(function (ProductSubscription $record) {
+                    $record->is_paid = true;
+                    $record->save();
+
+                    Notification::make()
+                        ->title('Order Approved')
+                        ->success()
+                        ->body('The order has been approved successfully')
+                        ->send();
+                })
+                ->color('Success')
+                ->requiresConfirmation()
+                ->visible(fn (ProductSubscription $record) => !$record->is_paid),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
