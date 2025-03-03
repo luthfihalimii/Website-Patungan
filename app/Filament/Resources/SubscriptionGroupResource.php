bc018be5bbeabdc1a81bd7908753fa1e521dfa6f
luthfihalimii <qlatifah62@gmail.com>
2025-03-03 07:29:43 +0700

    Coding CRUD grup patungan

--- app/Filament/Resources/SubscriptionGroupResource.php
+++ app/Filament/Resources/SubscriptionGroupResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\SubscriptionGroupResource\Pages;
 use App\Filament\Resources\SubscriptionGroupResource\RelationManagers;
+use App\Models\Product;
 use App\Models\SubscriptionGroup;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -23,7 +24,49 @@ class SubscriptionGroupResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Select::make('product_id')
+                ->relationship('product', 'name')
+                ->searchable()
+                ->preload()
+                ->required()
+                ->live()
+                ->afterStateUpdated(function ($state, callable $set ) {
+
+                    $product = Product::find($state);
+                    $max_capacity = $product ? $product->capacity : 0;
+
+                    $set('max_capacity', $max_capacity);
+                })
+
+                ->afterStateHydrated(function (callable $get, callable $set, $state) {
+                    $productId = $state;
+                    if ($productId) {
+                        $product = Product::find($productId);
+                        $max_capacity = $product ? $product->capacity : 0;
+
+                        $set('max_capacity', $max_capacity);
+                    }
+                }),
+
+                Forms\Components\TextInput::make('max_capacity')
+                ->required()
+                ->label('Max Capacity')
+                ->readOnly()
+                ->numeric()
+                ->prefix('People'),
+
+                Forms\Components\TextInput::make('participants_count')
+                ->required()
+                ->label('Total Capacity')
+                ->numeric()
+                ->prefix('People'),
+
+                Forms\Components\Select::make('product_subscription_id')
+                ->relationship('productSubscription', 'booking_trx_id')
+                ->searchable()
+                ->preload()
+                ->required(),
+
             ]);
     }
 
@@ -31,7 +74,30 @@ class SubscriptionGroupResource extends Resource
     {
         return $table
             ->columns([
-                //
+
+                Tables\Columns\ImageColumn::make('product.thumbnail')
+                ->label('photo')
+
+                Tables\Columns\TextColumn::make('productSubscription.booking_trx_id')
+                ->label('Booking ID')
+                ->searchable(),
+
+                Tables\Columns\TextColumn::make('id')
+                ->label('Group ID')
+                ->searchable(),
+
+                Tables\Columns\TextColumn::make('partcipants_count'),
+
+                Tables\Columns\TextColumn::make('max_capacity'),,
+
+                Tables\Columns\IconColumn::make('is_full')
+                ->label('full')
+                ->boolean()
+                ->getStateUsing(fn ($record) => $record->partcipants_count >= $record->max_capacity)
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->trueColor('success')
+                ->falseColor('danger'),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
