f9d77a68af428ce1342af6b96eb62e5fda84eb74
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-05-22 19:07:54 +0700

    Implement product details page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -20,8 +20,12 @@ class FrontController extends Controller
         return view('front.index', $data);
     }
 
-    public function details(Product $product){
-        return view('front.details', compact('product'));
+    public function details(Product $product)
+    {
+        $tax_percent = 11.0;
+        $tax_mult = $tax_percent / 100;
+        $tax = $product->price_per_person * $tax_mult;
+        $total_price = $product->price_per_person + $tax;
+        return view('front.details', compact('product', 'tax_percent', 'tax', 'total_price'));
     }
-
 }
